aead0cc1c8a16b5ea6821fa6d3d64a66ff43d203
董奕阳 <54069453+YYDanielJr@users.noreply.github.com>
2023-05-04 11:33:34 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
-g++ -c DataBase.cpp -lpthread
-g++ -c Logger.cpp -lpthread
-g++ -c DataProcessor.cpp -lpthread
-g++ -c TcpWork.cpp -lpthread
-g++ -c main.cpp -lpthread
-g++ main.o TcpWork.o Logger.o DataProcessor.o DataBase.o -o dyykvdb -lpthread
-rm TcpWork.o main.o Logger.o DataProcessor.o DataBase.o
+cmake .
+make
